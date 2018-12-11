2b47301f45beb88ab666de2e38db614b6caafbc2
srsedk17 <43095556+srsedk17@users.noreply.github.com>
2018-12-11 13:22:29 +0000

    Update json_check.sh

--- json_check.sh
+++ json_check.sh
@@ -2,8 +2,17 @@ perldoc -l JSON
 if [ $? != 0 ]
 then
 tar xvf JSON-2.97001.tar.gz
+sleep 5
 cd JSON-2.97001
 perl Makefile.PL
 make
+sleep 5
 make install
-fi 
+sleep 10
+if [ $? = 0 ]
+then
+echo "JSON module installed\n"
+fi
+else
+echo "JSON module existing\n"
+fi
